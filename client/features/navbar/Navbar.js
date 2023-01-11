41a34e1684921770ad7f7639d808b6298b193202
Rachel Eckert <rachel.eckert13@gmail.com>
2023-01-11 15:36:19 -0500

    navbar working and somewhat styled

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -6,7 +6,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { AppBar } from "@mui/material";
-import { Button } from "@mui/material";
+
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
@@ -18,29 +18,40 @@ const Navbar = () => {
 
   return (
     <Box sx={{ flexGrow: 1, mt: 0 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ bgcolor: "#ffc300" }}>
         <Toolbar>
-          <Typography variant="h5">Everything Honey</Typography>
+          <Typography variant="h5" sx={{ color: "#000000" }}>
+            Everything Honey
+          </Typography>
           <nav>
             {isLoggedIn ? (
               <div>
                 {/* The navbar will show these links after you log in */}
 
-                <Typography component={Link} to="/home" sx={{ mr: 3, ml: 3 }}>
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  to="/home"
+                  sx={{ mr: 3, ml: 3 }}>
                   Home
                 </Typography>
 
-                <Typography component={Link} to="/products">
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  to="/products"
+                  sx={{ mr: 3, ml: 3 }}>
                   Products
                 </Typography>
 
-                <Button
-                  display="flex"
-                  variant="text"
-                  type="button"
-                  onClick={logoutAndRedirectHome}>
+                <Typography
+                  variant="h5"
+                  sx={{ mr: 3, ml: 3 }}
+                  component={Link}
+                  onClick={logoutAndRedirectHome}
+                  className="logout-button">
                   Logout
-                </Button>
+                </Typography>
               </div>
             ) : (
               <div>
