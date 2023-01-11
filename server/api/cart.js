cde37f847fc80b890128508fe3478f99090f7395
NatalieRicci <ndeyoung@gmail.com>
2023-01-11 12:44:04 -0800

    beginning framework for cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
-const { Cart, Product, User } = require('../db')
+const { current } = require('@reduxjs/toolkit');
+const { Cart, Product, User } = require('../db');
+const Order = require('../db/models/Order');
 module.exports = router
 
+//view the entire cart
 router.get('/', async (req, res, next) => {
   try {
     const cart = await Cart.findAll()
@@ -11,11 +14,40 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+//how do we determine if the user is logged in or not?
+
+//update the product amount
 router.put('/', async (req, res, next) => {
   try {
-    const product = await Product.findByPk(req.params.id)
+    const product = await Cart.findByPk(req.params.productId)
     res.send(await Cart.update(product));
   } catch (err) {
     next(err)
   }
-})
+});
+
+//'checkout'--move from cart to order
+router.put('/checkout', async (req, res, next) => {
+  try {
+    const currentOrder = await Cart.findAll();
+    if (currentOrder) {
+      const newOrder = await Order.create(currentOrder);
+      res.json(newOrder)
+    } else {
+      console.log('Oops, looks like your cart is empty!')
+    }
+  } catch (err) {
+    next(err)
+  }
+});
+
+//delete item from cart
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const deletedProduct = await Cart.findByPk(req.params.productId);
+    await deletedProduct.destroy();
+    res.send(req.params.productId)
+  } catch (err) {
+    next(err)
+  }
+});
--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { models: { Product }} = require('../db')
+const { Product } = require('../db')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
