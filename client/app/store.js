527317ce2a1b7645fc6aa4dec8e2b3dcb51c9224
Lindsay Powell <lindsayerin3393@gmail.com>
2023-01-11 15:45:03 -0500

    another store edit

--- client/app/store.js
+++ client/app/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import authReducer from "../features/auth/authSlice";
 import productsReducer from "../features/products/productsSlice";
-import shoppingCartSlice from "../shoppingCart/shoppingCartSlice";
+import shoppingCartReducer from "../shoppingCart/shoppingCartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
-    cart: shoppingCartSlice,
+    carts: shoppingCartReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
